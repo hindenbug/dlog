806918a81fec0c45958421bb4467386c81b489d9
hindenbug <304184+hindenbug@users.noreply.github.com>
2021-08-20 19:37:59 +0200

    Add Store for logs, append, read functions

---
+++ internal/log/store.go
@@ -0,0 +1,98 @@
+package log
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+const (
+	limit = 8
+)
+
+type store struct {
+	*os.File
+	mu     sync.Mutex
+	buffer *bufio.Writer
+	size   uint64
+}
+
+func newStore(f *os.File) (*store, error) {
+	file, err := os.Stat(f.Name())
+
+	if err != nil {
+		return nil, err
+	}
+
+	size := uint64(file.Size())
+	return &store{File: f, size: size, buffer: bufio.NewWriter(f)}, nil
+
+}
+
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+
+	// write the binary representation of the data into the bufio.Writer
+	// we first tell how much data we're going to write, with what encoding format
+	if err := binary.Write(s.buffer, binary.BigEndian, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+
+	// Write the Record data
+	w, err := s.buffer.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// The number of written bytes is the Record length + the written Record data size
+	w += limit
+	s.size += uint64(w)
+
+	return uint64(w), pos, nil
+}
+
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	// flush the buffered writer so we don't read a record that hasn't been flushed to disk yet
+	if err := s.buffer.Flush(); err != nil {
+		return nil, err
+	}
+	// The amount of bytes needed to read the whole record are determined
+	size := make([]byte, limit)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+	// fetching and retuning the record.
+	b := make([]byte, binary.BigEndian.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+limit)); err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func (s *store) ReadAt(p []byte, offset int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buffer.Flush(); err != nil {
+		return 0, err
+	}
+
+	return s.File.ReadAt(p, offset)
+}
+
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	err := s.buffer.Flush()
+	if err != nil {
+		return err
+	}
+
+	return s.File.Close()
+}
---
+++ internal/log/store_test.go
@@ -0,0 +1,108 @@
+package log
+
+import (
+	"encoding/binary"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	testData = []byte("hello world")
+	width    = limit + uint64(len(testData))
+)
+
+func TestStoreAppendRead(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	//s, err = newStore(f)
+	//require.NoError(t, err)
+}
+
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(testData)
+
+		require.NoError(t, err)
+		require.Equal(t, pos+n, width*i)
+	}
+}
+
+func testRead(t *testing.T, s *store) {
+	t.Helper()
+	var pos uint64
+	for i := uint64(1); i < 4; i++ {
+		read, err := s.Read(pos)
+		require.NoError(t, err)
+		require.Equal(t, testData, read)
+		pos += width
+	}
+}
+
+func testReadAt(t *testing.T, s *store) {
+	t.Helper()
+	for i, offset := uint64(1), int64(0); i < 4; i++ {
+		b := make([]byte, limit)
+		n, err := s.ReadAt(b, offset)
+		require.NoError(t, err)
+		require.Equal(t, limit, n)
+		offset += int64(n)
+
+		size := binary.BigEndian.Uint64(b)
+		b = make([]byte, size)
+		n, err = s.ReadAt(b, offset)
+		require.NoError(t, err)
+		require.Equal(t, testData, b)
+		require.Equal(t, int(size), n)
+		offset += int64(n)
+	}
+}
+
+func TestStoreClose(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_close_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+	s, err := newStore(f)
+	require.NoError(t, err)
+	_, _, err = s.Append(testData)
+	require.NoError(t, err)
+
+	f, beforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+
+	err = s.Close()
+	require.NoError(t, err)
+
+	_, afterSize, err := openFile(f.Name())
+	require.NoError(t, err)
+	require.True(t, afterSize > beforeSize)
+
+}
+
+func openFile(name string) (file *os.File, size int64, err error) {
+	file, err = os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+
+	if err != nil {
+		return nil, 0, err
+	}
+
+	fileInfo, err := file.Stat()
+
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return file, fileInfo.Size(), nil
+}
